754a3127e97214256ff830f7fcff080707ea5fd0
philippe-lacaze <philippe.lacaze@free.fr>
2018-04-24 15:49:36 +0200

    Ajout jsdoc

--- src/app/services/berlin-clock-row.ts
+++ src/app/services/berlin-clock-row.ts
@@ -1,3 +1,11 @@
+/**
+ * Interface commune des services donnant une row de la berlin clock.
+ */
 export interface BerlinClockRow {
+  /**
+   * Retourne la row pour la date donnée.
+   * @param {Date} date donnée.
+   * @returns {string} la row pour la date donnée.
+   */
   giveRow(date: Date): string;
 }
